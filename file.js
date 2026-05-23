4bac3d0ab62e34fb58f58de21c2adc899472039a
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-05-23 21:19:50 +0530

    updates-file-js

--- file.js
+++ file.js
@@ -1,1 +1,17 @@
-``
+// fs - File System
+const fs = require("fs");
+
+// 3 parameters
+// FileName
+// File Content
+// Callback Function
+fs.writeFile("entri.txt","Hello It's a Entri Live Session",(err) => {
+if(err){
+    console.log("File Creation Failed");
+    return;
+}
+console.log("File Operation Successful");
+});
+
+// after put node file.js in terminal & enter
+// Result ->   "File Operation Successful" visible  at the sametime new file automatically created with typed text  
