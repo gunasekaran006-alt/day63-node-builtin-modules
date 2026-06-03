9f6ea2a4b862e900b4cfde59294d392ec0b53f58
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-06-03 22:14:46 +0530

    updates

--- events.js
+++ events.js
@@ -2,25 +2,26 @@
 const EventEmitter = require("events");
 
 // Scenario 1 - Chat Application (zoho)
-const chatEvents = new EventEmitter(); 
+const chatEvents = new EventEmitter();
 
 // Group area
-chatEvents.on("newMessage",(user,message) => {
-console.log(`New Message from ${user} : ${message}`);
+chatEvents.on('newMessage', (user, message) => {
+    console.log(`New Message from ${user}: ${message}`);
 });
-chatEvents.emit("newMessage", "Guna" , "Kindly join the session" ); // New Message from Guna : Kindly join the session
-chatEvents.emit("newMessage", "Sekar", "Joining sir"); // New Message from Sekar : Joining sir
+
+chatEvents.emit('newMessage', 'Narasimhan', 'Kindly join the session');
+chatEvents.emit('newMessage', 'Guna', 'Joining sir');
 
 // Scenario 2 - Payment (Razorpay, BHIM, PayPal)
 const paymentEvent = new EventEmitter();
 
-paymentEvent.once("processPayment", (amount) => {
+paymentEvent.once('processPayment', (amount) => {
     console.log(`Payment of ${amount} processed successfully`);
 });
 
-console.log("User Clicked the Pay Button"); // just for understanding purpose only this
-paymentEvent.emit("processPayment", 5000);
+console.log("User clicked the pay button"); // User Clicked the Pay Button
+paymentEvent.emit('processPayment', 5000); // Payment of 5000 processed successfully
 
-console.log("User Clicked the Pay Button Again"); // just for understanding purpose only this
-paymentEvent.emit("processPayment", 5000);
+console.log("User clicked the pay button again") // User Clicked the Pay Button Again
+paymentEvent.emit('processPayment', 5000);
 // second entry - it will automatically be ignored
